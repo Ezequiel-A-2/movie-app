0ab9d4896776e54799ea4ba814101e581eb7382d
Ezequiel Ricardo Alarcon <87947119+Ezequiel-A-2@users.noreply.github.com>
2021-08-31 12:06:16 -0300

    Update home.js

--- Scripts/home.js
+++ Scripts/home.js
@@ -1,12 +1,12 @@
 const FILMS_2D = [
-    { portada: "/resources/img/space-jam.jpg", titulos: "Space Jam"}, 
-    { portada: "/resources/img/placeholder_img.jpg", titulos: "Pelicula 2" },
-    { portada: "/resources/img/placeholder_img.jpg", titulos: "Pelicula 3" },
-    { portada: "/resources/img/placeholder_img.jpg", titulos: "Pelicula 4" },
-    { portada: "/resources/img/placeholder_img.jpg", titulos: "Pelicula 5" },
-    { portada: "/resources/img/placeholder_img.jpg", titulos: "Pelicula 6" },
-    { portada: "/resources/img/placeholder_img.jpg", titulos: "Pelicula 7" },
-    { portada: "/resources/img/placeholder_img.jpg", titulos: "Pelicula 8" }, 
+    { portada: "../resources/img/space-jam.jpg", titulos: "Space Jam"}, 
+    { portada: "../resources/img/placeholder_img.jpg", titulos: "Pelicula 2" },
+    { portada: "../resources/img/placeholder_img.jpg", titulos: "Pelicula 3" },
+    { portada: "../resources/img/placeholder_img.jpg", titulos: "Pelicula 4" },
+    { portada: "../resources/img/placeholder_img.jpg", titulos: "Pelicula 5" },
+    { portada: "../resources/img/placeholder_img.jpg", titulos: "Pelicula 6" },
+    { portada: "../resources/img/placeholder_img.jpg", titulos: "Pelicula 7" },
+    { portada: "../resources/img/placeholder_img.jpg", titulos: "Pelicula 8" }, 
 ]
 
 const peliculas2D = document.getElementById("film-container")
