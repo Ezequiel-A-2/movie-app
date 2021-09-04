32dd3953c5a942e71aedd541b8e9bae55aa8691a
Ezequiel Alarcon <ps3ezequiel@gmail.com>
2021-09-04 02:51:38 -0300

    actualizando la pestaña admi con jquery

--- Scripts/admi.js
+++ Scripts/admi.js
@@ -1,6 +1,32 @@
 let nuevaPeli
 let portada
 
+// === Declaracion de constantes ===
+
+const boton = $(`#finish-button`).click(getData)
+
+function getData() {
+    const _titulos = $(`#title`).val()
+    const _portada = $(`#basic-url`).val()
+    
+    console.log(_titulos, _portada)
+}
+
+function test() {
+    let checkboxes = $(`:checkbox`)
+
+    for (box of checkboxes) {
+        $(box).prop("checked") === true ? // Creaar un swith o ver la forma de capturar el valor del checkbox
+        console.log(box.value)
+        : console.log("false")
+    }
+
+
+
+}
+
+
+
 
 
 
@@ -24,7 +50,7 @@ agregarPelicula()
 
 nuevaPeli = { portada, titulos: "Ejemplo", film2D: false, film3D: true }
 
-export default nuevaPeli;
+// export default nuevaPeli;
 
 
 
--- Scripts/ejemplo.js
+++
@@ -1,5 +0,0 @@
-import pepe from "./admi.js";
-import FILMS from "./homeDB.js"
-
-console.log(pepe)
-console.table(FILMS)
