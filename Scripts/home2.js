6d82d78a29f594f844e78afa86996ba5e7ac501d
Ezequiel Alarcon <ps3ezequiel@gmail.com>
2021-09-16 00:13:50 -0300

    Arreglos en el Home para imprimir datos en pantalla

--- Scripts/home2.js
+++ Scripts/home2.js
@@ -69,6 +69,40 @@ function movieFilter(arrayDB) {
     movieFilter(movieDB)
 })()
 
-if (sessionStorage.getItem(`newData`)) {
-    storageMovie(allMovies)
+if (sessionStorage.getItem('newData')) {
+    const storageMovies = JSON.parse(sessionStorage.getItem('newData'))
+    // newCard(storageMovies)
+}
+
+// generar cards adicionales
+function newCard (arrayOfNewFilms) {
+    // verificar si es 2D o 3D y hay hacer la insercion
+
+    
+    for (let film of arrayOfNewFilms) {
+        const plantilla = `
+            <div class="col">
+                <div class="card">
+                    <a href="../asientos2.html">
+                        <img src="${film.image}" class="card-img-top skeleton" alt="Portada de ${film.title}" data-movie-id=${film.id}>
+                    </a>
+                    <div class="card-body pt-2">
+                        <h3 class="card-title text-center">
+                            ${film.title}
+                        </h3>
+                    </div>
+                </div>
+            </div>`
+
+        let newMovie = document.createElement("div")
+        newMovie.setAttribute("class", "cartas") 
+        newMovie.innerHTML = plantilla
+        console.log(film)
+        /* if () {
+            _film2D
+            _film3D
+        } */
+        // section.appendChild(pelicula)
+    }
+
 }
